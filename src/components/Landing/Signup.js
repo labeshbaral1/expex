60302fe372650cceb3104dd74d49545580092485
syadav7173 <shubhamyadavny@outlook.com>
2022-12-24 21:54:34 -0500

    signup page ui update

--- src/components/Landing/Signup.js
+++ src/components/Landing/Signup.js
@@ -14,38 +14,54 @@ function Signup() {
 
   return (
     <div className="signup">
-      <h1> Expex Sign Up</h1>
-
-      <form
-        className="signup-form "
-        onSubmit={(event) => {
-          event.preventDefault();
-          signupUser(name, email, password, navigate);
-        }}
-      >
-        <input
-          type="text"
-          id="name"
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => updateName(e.target.value)}
-        />
-        <input
-          type="text"
-          id="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => updateEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          id="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => updatePassword(e.target.value)}
-        />
-        <button>Sign Up</button>
-      </form>
+      <div className='top-logo2'>Expex</div>
+      <div className="signup-cont">
+        <form
+          className="signup-form "
+          onSubmit={(event) => {
+            event.preventDefault();
+            signupUser(name, email, password, navigate);
+          }}
+        >
+          <div className="intro-text">
+            Sign Up.
+          </div>
+          <div className='small-name'>Username</div>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => updateName(e.target.value)}
+            className='up'
+          />
+          <div className='small-email2'>Email</div>
+          <input
+            type="text"
+            id="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => updateEmail(e.target.value)}
+            className='up'
+          />
+          <div className='small-pass2'>Password (6-25 characters)</div>
+          <input
+            type="text"
+            id="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => updatePassword(e.target.value)}
+            className='up'
+          />
+          <button className="signup-but">Sign Up</button>
+          <div className="divider-tot">
+          <div className="divider1">_______________</div>
+          <div className="divider2">_______________</div>
+          </div>
+          <div className="or">or</div>
+          <button className='goog2-reg'>Sign up with Google</button>
+        </form>
+      </div>
     </div>
   );
 }
