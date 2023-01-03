1249e3cc3e0681e031ea1cf844377fd8b8b860c7
Labesh Baral <99603007+labeshbaral1@users.noreply.github.com>
2023-01-02 19:21:15 -0500

    balance completee

--- server/index.js
+++ server/index.js
@@ -145,7 +145,7 @@ app.post("/api/main", function (request, response, next) {
 
     const transactionRequest = await client.transactionsGet({
       access_token: accessToken,
-      start_date: moment().startOf('month').format('YYYY-MM-DD'),
+      start_date: ('2022-12-01'),
       end_date: moment().format('YYYY-MM-DD')
     });
     
