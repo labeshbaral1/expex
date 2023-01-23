d6c7b7b48a2b0c78c530bf00d5e9880ea8a2ff5c
Labesh Baral <99603007+labeshbaral1@users.noreply.github.com>
2023-01-22 23:47:22 -0500

    Revert "invest tile updates"

--- src/About.js
+++ src/About.js
@@ -173,22 +173,22 @@ function About() {
       </div>
 
       <div className="split right">
-        <img className="hand" src={bob} alt="" />{" "}
-        {/* <img className="phone" src={"/assets/srikar.png"} /> */}
-      </div>
-
-      <div className="split right">
-      <Link to="/overview" onClick={() => handleTabClick("overview")}>
+        <Link to="/overview" onClick={() => handleTabClick("overview")}>
           <div className="close-cont">
             <CloseIcon
               className="close"
               fontSize="large"
-              style={{ color: "var(--text-color)" }}
+              style={{ color: "black" }}
             />
           </div>
         </Link>
         <img className="hand" src={bob} alt="" />{" "}
         {/* <img className="phone" src={"/assets/srikar.png"} /> */}
+      </div>
+
+      <div className="split right">
+        <img className="hand" src={bob} alt="" />{" "}
+        <img className="phone" src={"/assets/srikar.png"} />
         {firstTimeLogin && (
           <>
             <div className="continue" onClick={()=> navigate("/linkAccount")}>
@@ -196,7 +196,6 @@ function About() {
             </div>
           </>
         )}
-        <CloseIcon/>
       </div>
     </div>
   );
